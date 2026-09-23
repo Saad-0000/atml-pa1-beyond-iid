e17c5eefd17372b9d228b96e68262c13120ca6b0
Saad-0000 <muhammadsaad.00@outlook.com>
2026-09-23 15:51:33 +0500

    train.py for task 3 modified

--- task3/evaluation/evaluate_sketch.py
+++ task3/evaluation/evaluate_sketch.py
@@ -1,4 +1,7 @@
 import argparse
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 import torch
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
